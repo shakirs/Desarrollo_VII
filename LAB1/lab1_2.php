5622406e395a422304fd8d8de9d657f129bc108e
Shakirs Franco <kirsshacofran@gmail.com>
2022-09-04 00:55:54 -0500

    git commit -m "lab1_2.php"

---
+++ LAB1/lab1_2.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lab 1.1</title>
+</head>
+<body>
+    <?php
+    $ini1 = 1;
+    $ini2 = 2;
+    $suma = $ini1 + $ini2;
+    echo $suma;
+    ?>
+</body>
+</html>
