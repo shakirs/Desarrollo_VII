b3c3106f2f0e9f4d9e9e68bdbd501dfe0c94a465
Shakirs Franco <kirsshacofran@gmail.com>
2022-09-04 00:46:39 -0500

    git commit -m "Mi primer commit"

---
+++ LAB1/lab11.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lab 1.1</title>
+</head>
+<body>
+    <?php
+    $initial = "Hola";
+    $final = "A todos";
+    $todo = $initial . " " . $final;
+    echo $todo;
+    ?>
+</body>
+</html>
