9affba7a6041b459db335115fd5a487a7ac95a82
Shakirs Franco <kirsshacofran@gmail.com>
2022-09-04 01:04:46 -0500

    git commit -m "lab1.2"

--- LAB1/lab1_3.php
+++ LAB1/lab1_3.php
@@ -3,14 +3,14 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lab 1.2</title>
+    <title>Lab 1.1</title>
 </head>
 <body>
     <?php
-    $ini1 = 1;
-    $ini2 = 2;
-    $suma = $ini1 + $ini2;
-    echo $suma;
+    $initial = "Hola";
+    $final = "A todos"; 
+    $todo = $initial . " " . $final;
+    echo $todo;
     ?>
 </body>
 </html>
